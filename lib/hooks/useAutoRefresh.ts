475e10f57cfc32d759f4aa9379e6e976a2d37f8c
Gilson Pimentel <dev@doohplay.com.br>
2026-05-22 12:39:59 -0300

    fix: restore lib/hooks/useAutoRefresh.ts at root for @/ alias

---
+++ lib/hooks/useAutoRefresh.ts
@@ -0,0 +1,10 @@
+"use client"
+
+import { useEffect } from 'react'
+
+export function useAutoRefresh(callback: () => void, intervalMs = 30000) {
+  useEffect(() => {
+    const interval = setInterval(callback, intervalMs)
+    return () => clearInterval(interval)
+  }, [callback, intervalMs])
+}
