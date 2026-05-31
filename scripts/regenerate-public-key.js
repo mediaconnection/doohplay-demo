ae06cf6e58b8bcedf77ffa9f48f0a3850dfb578f
Gilson Pimentel <dev@doohplay.com.br>
2026-05-31 10:51:59 -0300

    feat: script para regenerar public.pem do PRIVATE_PEM no startup

---
+++ scripts/regenerate-public-key.js
@@ -0,0 +1,9 @@
+﻿const crypto = require('crypto')
+const fs = require('fs')
+const privRaw = process.env.PRIVATE_PEM
+if (privRaw) {
+  const priv = privRaw.startsWith('-----') ? privRaw : '-----BEGIN PRIVATE KEY-----\n' + privRaw.match(/.{1,64}/g).join('\n') + '\n-----END PRIVATE KEY-----\n'
+  const pub = crypto.createPublicKey(priv).export({ type: 'spki', format: 'pem' }).toString()
+  fs.writeFileSync('keys/public.pem', pub)
+  console.log('[startup] public.pem regenerado do PRIVATE_PEM')
+}
